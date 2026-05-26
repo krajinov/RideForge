072f6e50f37893cc682413ebcabd30b895c3f0cf
Josip Krajinovic <josip.krajinovic@multibank.io>
2026-05-26 13:13:24 +0200

    Fix bottom sheet navigation bar overlap on Android 10

--- composeApp/src/commonMain/kotlin/com/delminiusapps/rideforge/features/profile/presentation/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/delminiusapps/rideforge/features/profile/presentation/ProfileScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -485,6 +486,7 @@ private fun EditProfileSheet(
     ModalBottomSheet(
         onDismissRequest = { if (!isSaving) onDismiss() },
         containerColor = ForgeSurface,
+        windowInsets = WindowInsets(0, 0, 0, 0),
     ) {
         Column(
             modifier = Modifier
